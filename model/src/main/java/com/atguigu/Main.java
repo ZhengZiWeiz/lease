390b19ffd0cfe9816dd531f7422555c23776a469
zzw <250502>
2024-12-18 12:48:37 +0800

    hot-fix 合并冲突

--- model/src/main/java/com/atguigu/Main.java
+++ model/src/main/java/com/atguigu/Main.java
@@ -14,6 +14,7 @@ public class Main {
             System.out.println("i = " + i);
             System.out.println("ssss");
             System.out.println("1111");
+            System.out.println("2222");
         }
     }
 }
